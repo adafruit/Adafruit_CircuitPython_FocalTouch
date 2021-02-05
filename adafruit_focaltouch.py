0036d9caf66b9560831bc2909a9236a59ab79d01
Kevin Matocha <kmatocha@icloud.com>
2021-02-04 20:23:28 -0600

    Add option for IRQ pin to wait for _read

--- adafruit_focaltouch.py
+++ adafruit_focaltouch.py
@@ -67,11 +67,12 @@ class Adafruit_FocalTouch:
     _debug = False
     chip = None
 
-    def __init__(self, i2c, address=_FT6206_DEFAULT_I2C_ADDR, debug=False):
+    def __init__(self, i2c, address=_FT6206_DEFAULT_I2C_ADDR, debug=False, irq_pin=None):
         self._i2c = I2CDevice(i2c, address)
         self._debug = debug
+        self.irq_pin = irq_pin
 
-        chip_data = self._read(_FT6XXX_REG_LIBH, 8)
+        chip_data = self._read(_FT6XXX_REG_LIBH, 8) # don't wait for IRQ
         lib_ver, chip_id, _, _, firm_id, _, vend_id = struct.unpack(
             ">HBBBBBB", chip_data
         )
@@ -90,10 +91,11 @@ class Adafruit_FocalTouch:
             print("Point rate %d Hz" % self._read(_FT6XXX_REG_POINTRATE, 1)[0])
             print("Thresh %d" % self._read(_FT6XXX_REG_THRESHHOLD, 1)[0])
 
+
     @property
     def touched(self):
         """ Returns the number of touches currently detected """
-        return self._read(_FT6XXX_REG_NUMTOUCHES, 1)[0]
+        return self._read(_FT6XXX_REG_NUMTOUCHES, 1, irq_pin=self.irq_pin)[0]
 
     # pylint: disable=unused-variable
     @property
@@ -103,7 +105,7 @@ class Adafruit_FocalTouch:
         touch coordinates, and 'id' as the touch # for multitouch tracking
         """
         touchpoints = []
-        data = self._read(_FT6XXX_REG_DATA, 32)
+        data = self._read(_FT6XXX_REG_DATA, 32, irq_pin=self.irq_pin)
 
         for i in range(2):
             point_data = data[i * 6 + 3 : i * 6 + 9]
@@ -121,11 +123,17 @@ class Adafruit_FocalTouch:
 
     # pylint: enable=unused-variable
 
-    def _read(self, register, length):
+    def _read(self, register, length, irq_pin=None):
         """Returns an array of 'length' bytes from the 'register'"""
         with self._i2c as i2c:
+
+            if (irq_pin is not None):
+                while (self.irq_pin.value):
+                    pass
+
             i2c.write(bytes([register & 0xFF]))
             result = bytearray(length)
+
             i2c.readinto(result)
             if self._debug:
                 print("\t$%02X => %s" % (register, [hex(i) for i in result]))
